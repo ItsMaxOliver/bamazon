20f71d64dc4f47f3fb5f1d1928d37342673742f1
MollyLJohnson <mollyl.johnson@att.net>
2018-04-19 22:42:34 -0500

    finished functionality for placing an order and seeing the updated stock quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
@@ -5,7 +6,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "12345",
+    password: process.env.MYSQLPASS,
     database: "bamazon"
 });
 
@@ -13,19 +14,19 @@ connection.connect(function(err) {
     if (err) {
         throw err;
     }
-    else{
+    else {
         queryAllProducts();
-        connection.end();
     }
 })
 
 function queryAllProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
+        console.log("");
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].item_id + " | " + res[i].product_name + " | $" + res[i].price);
         }
-        console.log("------------------------------------------------\n");
+        console.log("\n------------------------------------------------\n");
         promptUser(res);
     })
 }
@@ -44,7 +45,26 @@ function promptUser(res) {
         for (var i = 0; i < res.length; i++) {
             if (parseInt(answers.id) === res[i].item_id) {
                 if (res[i].stock_quantity >= (parseInt(answers.quantity))) {
-                    console.log("order processing...");
+                    console.log("\norder processing...");
+                    
+                    var orderCost = parseFloat(res[i].price * answers.quantity);
+                    console.log("\n Your order total is: $" + orderCost.toFixed(2));
+                    console.log("\n------------------------------------------------");
+                    
+                    var newQuantity = res[i].stock_quantity - (parseInt(answers.quantity));
+                    
+                    connection.query("UPDATE products SET ? WHERE ?",
+                    [{
+                        stock_quantity : newQuantity 
+                    }, {
+                        item_id : answers.id
+                    }
+                    ], function(error, response) {
+                        if (error) console.log(error);
+                        else {
+                            printUpdatedProducts();
+                        }
+                    })
                 }
                 else {
                     console.log("\nI'm sorry we don't have that many products in stock.\n");
@@ -53,10 +73,21 @@ function promptUser(res) {
             }
         }
     })
-        //update sql databse and then log how much the total cost of their order is
     .catch(function(err) {
         if (err) {
             console.log(err);
         }
     })
+}
+
+function printUpdatedProducts() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.log("");
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].product_name + " | Quantity left: " + res[i].stock_quantity);
+        }
+        console.log("\n------------------------------------------------\n");
+    })
+    connection.end();
 }
